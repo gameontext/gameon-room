40e8b2a6bef004d3d8d4b2117e41c46546ae41d5
Erin Schnabel <ebullient@users.noreply.github.com>
2015-10-24 11:41:09 -0400

    Adjustments to Concierge endpoint (rest paths, 404)

--- concierge-app/src/main/java/net/wasdev/gameon/concierge/Concierge.java
+++ concierge-app/src/main/java/net/wasdev/gameon/concierge/Concierge.java
@@ -6,33 +6,64 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+<<<<<<< Upstream, based on origin/manualWiring
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.Application;
 
+=======
+import net.wasdev.gameon.room.common.Endpoint;
+import net.wasdev.gameon.room.common.EndpointCollection;
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 import net.wasdev.gameon.room.common.RegistrationResponse;
 import net.wasdev.gameon.room.common.Room;
 import net.wasdev.gameon.room.common.RoomToEndpoints;
 
+<<<<<<< Upstream, based on origin/manualWiring
 @ApplicationScoped
 public class Concierge extends Application {
+=======
+public class Concierge {
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 
+<<<<<<< Upstream, based on origin/manualWiring
 	Map<String, RoomToEndpoints> roomDirectory = new HashMap<String, RoomToEndpoints>();
 	Set<String> startingRooms = new HashSet<String>();
 	
+=======
+	Map<UUID, Room> roomDirectory = new HashMap<UUID, Room>();
+	Map<String, List<Room>> roomNameToCollection = new HashMap<String, List<Room>>();
+	Room startingRoom = null;
+
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 	PlacementStrategy ps = new Simple2DPlacement();
-	
+
 	public Concierge(PlacementStrategy placementStrategy) {
-			ps = placementStrategy;
+		ps = placementStrategy;
 	}
 
 	public Concierge() {
 	}
 
+<<<<<<< Upstream, based on origin/manualWiring
 	public RoomToEndpoints getStartingRoom() {
 		for (String roomId : startingRooms) {
 			RoomToEndpoints roomCollection = roomDirectory.get(roomId);
 			if (roomCollection != null) {
 				return roomCollection;
+=======
+	public Room getStartingRoom() {
+		return startingRoom;
+	}
+
+	public EndpointCollection exitRoom(String currentRoomId, String exitName) {
+		List<Room> nextRooms = ps.getConnectingRooms(currentRoomId, exitName);
+		List<Endpoint> endpoints = new ArrayList<Endpoint>();
+		if (nextRooms != null) {
+			for (Room room : nextRooms) {
+				Endpoint endpoint = new Endpoint();
+				endpoint.setUrl(room.getAttribute("endPoint"));
+				endpoints.add(endpoint);
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 			}
 		}
 		return null; 
@@ -45,6 +76,7 @@ public class Concierge extends Application {
 
 	public RegistrationResponse registerRoom(Room room) {
 		System.out.println("Processing registration for : \n" + room.toString());
+<<<<<<< Upstream, based on origin/manualWiring
 		
 		RoomToEndpoints rte = roomDirectory.get(room.getRoomName());
 		if (rte == null) {
@@ -68,13 +100,34 @@ public class Concierge extends Application {
 			startingRooms.add(room.getRoomName());
 		}
 		RegistrationResponse rr = new RegistrationResponse();
+=======
+		if (startingRoom == null) {
+			startingRoom = room;
+		}
+		List<Room> roomMap = roomNameToCollection.get(room.getRoomName());
+		if (roomMap == null) {
+			roomMap = new ArrayList<Room>();
+		}
+		roomMap.add(room);
+		roomNameToCollection.put(room.getRoomName(), roomMap);
+		UUID roomUUID = UUID.randomUUID();
+		room.setAssignedID(roomUUID);
+		roomDirectory.put(roomUUID, room);
+		ps.placeRoom(room);
+
+		RegistrationResponse rr = new RegistrationResponse(roomUUID);
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 		return rr;
 	}
 
+<<<<<<< Upstream, based on origin/manualWiring
 	public RoomToEndpoints getRoom(String roomId) {
 		RoomToEndpoints rte = new RoomToEndpoints();
 		return null;
 	}
 	
 	
+=======
+
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 }
--- concierge-app/src/main/java/net/wasdev/gameon/concierge/ConciergeEndpoint.java
+++ concierge-app/src/main/java/net/wasdev/gameon/concierge/ConciergeEndpoint.java
@@ -1,8 +1,11 @@
 package net.wasdev.gameon.concierge;
 
+<<<<<<< Upstream, based on origin/manualWiring
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
+=======
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,29 +16,54 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import net.wasdev.gameon.room.common.EndpointCollection;
 import net.wasdev.gameon.room.common.Room;
 
+<<<<<<< Upstream, based on origin/manualWiring
 @ApplicationPath("")
 @Path("concierge")
 @ApplicationScoped
+=======
+@Path("/")
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 public class ConciergeEndpoint extends Application {
+<<<<<<< Upstream, based on origin/manualWiring
 	
 
 	@Inject
 	Concierge c;
+=======
+
+	Concierge c = new Concierge(new Simple2DPlacement());
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 
 	@GET
 	@Path("startingRoom")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getStartingRoom() {
-		return Response.ok(c.getStartingRoom()).build();
+		Room startingRoom = c.getStartingRoom();
+
+		if ( startingRoom == null )
+			return Response.status(404).build();
+
+		return Response.ok(startingRoom).build();
 	}
 	
 	@GET
 	@Path("rooms/{roomId}/{exitName}")
 	@Produces(MediaType.APPLICATION_JSON)
+<<<<<<< Upstream, based on origin/manualWiring
 	public Response exitRoom(@PathParam("roomId") String roomId, @PathParam("exitName") String exitName) {
 		return Response.ok(c.exitRoom(roomId, exitName)).build();
+=======
+	public Response exitRoom(@PathParam("roomId") String roomId, @PathParam("roomID") String exitName) {
+		EndpointCollection ec = c.exitRoom(roomId, exitName);
+
+		if ( ec.getEndpoints().isEmpty() )
+			return Response.status(404).build();
+
+		return Response.ok(ec).build();
+>>>>>>> 0cc092c Adjustments to Concierge endpoint (rest paths, 404)
 	}
 	
 	@GET
