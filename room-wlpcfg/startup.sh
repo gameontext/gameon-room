41356e93b13177adabaf4a3ffafdbd6b3de4833b
Ben Smith <benjsmi@us.ibm.com>
2015-11-05 17:53:03 -0500

    Incorrect value for room_service

--- room-wlpcfg/startup.sh
+++ room-wlpcfg/startup.sh
@@ -2,6 +2,6 @@
 export DOCKERHOST=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
 echo Found Docker host: $DOCKERHOST
 export service_concierge=http:\/\/$DOCKERHOST:9081/concierge/registerRoom
-export service_room=ws:\/\/$DOCKERHOST:9081/rooms
+export service_room=ws:\/\/$DOCKERHOST:9080/rooms
 /opt/ibm/wlp/bin/server run defaultServer
 
