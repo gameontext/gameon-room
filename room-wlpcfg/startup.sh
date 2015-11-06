e1d04c39284ce5186dc613561be228c78b660481
Ben Smith <benjsmi@us.ibm.com>
2015-11-06 15:53:30 -0500

    First room connection should be OK.

--- room-wlpcfg/startup.sh
+++ room-wlpcfg/startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-export DOCKERHOST=$(route -n | grep 'UG[ \t]' | awk '{print $2}')
-echo Found Docker host: $DOCKERHOST
-export service_concierge=http:\/\/$DOCKERHOST:9081/concierge/registerRoom
-export service_room=ws:\/\/$DOCKERHOST:9080/rooms
+export service_concierge=http:\/\/gameon-concierge:9081/concierge/registerRoom
+export service_room=ws:\/\/game-on.org/rooms
 /opt/ibm/wlp/bin/server run defaultServer
 
