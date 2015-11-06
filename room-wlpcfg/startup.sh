3c3068b02c21c7adeb467f550993233a6ea495ad
Ben Smith <benjsmi@us.ibm.com>
2015-11-06 15:55:11 -0500

    Fix those with build properties, not with hard-coded things in a script.

--- room-wlpcfg/startup.sh
+++ room-wlpcfg/startup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-export service_concierge=http:\/\/gameon-concierge:9081/concierge/registerRoom
-export service_room=ws:\/\/game-on.org/rooms
 /opt/ibm/wlp/bin/server run defaultServer
 
