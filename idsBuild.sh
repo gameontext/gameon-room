e502487ec833310a8e85e66683f25c960158143f
Ben Smith <benjsmi@us.ibm.com>
2015-11-09 08:02:17 -0500

    Forgot we'll actually be storing those in a separate directory.

--- idsBuild.sh
+++ idsBuild.sh
@@ -6,13 +6,14 @@
 
 #!/bin/bash
 echo Informing slack...
-curl -X 'POST' --silent --data-binary '{"text":"A new build for the room service has started."}' $WEBHOOK > /dev/null
+#curl -X 'POST' --silent --data-binary '{"text":"A new build for the room service has started."}' $WEBHOOK > /dev/null
 
 mkdir dockercfg ; cd dockercfg
 echo Downloading Docker requirements..
 wget http://docker-2.game-on.org:8081/dockerneeds.tar -q
 echo Setting up Docker...
-tar xzf dockerneeds.tar 
+tar xzf dockerneeds.tar
+cd .. 
 	 
 echo Downloading Java 8...
 wget http://game-on.org:8081/jdk-8u65-x64.tar.gz -q
