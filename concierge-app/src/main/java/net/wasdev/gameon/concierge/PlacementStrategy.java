02ddfdb1de2c267e72404ac7f8e3f102aae1e2f5
Ben Smith <benjsmi@us.ibm.com>
2015-12-07 08:32:25 -0500

    Concierge now has its own project and build stream, so let's take it out of here.

--- concierge-app/src/main/java/net/wasdev/gameon/concierge/PlacementStrategy.java
+++
@@ -1,9 +0,0 @@
-package net.wasdev.gameon.concierge;
-
-import net.wasdev.gameon.room.common.Room;
-
-public interface PlacementStrategy {
-	void placeRoom(Room room);
-
-	String getConnectingRooms(String currentRoomId, String exitName);
-}
